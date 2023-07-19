46e412199b57f25cd75396ee9f7a20d6135e3662
Jokibs <122084307+Jokibs@users.noreply.github.com>
2023-07-19 18:00:43 +0300

    Update elleb.js

--- elleb.js
+++ elleb.js
@@ -33,14 +33,43 @@ toggleButton2.addEventListener('click', function() {
 
     // Set the text in the display element
     displayElement.textContent = myText;
-document.getElementById("thalia-btn").addEventListener("click", function() {
-      var message = "Hello, I wanted to connect with you.";
+function createWhatsAppButtonListener(buttonId, message) {
+  var button = document.getElementById(buttonId);
+  if (button) {
+    button.addEventListener("click", function() {
       var encodedMessage = encodeURIComponent(message);
-      var phoneNumber = "+254114511439"; // Replace with the desired phone number
+      var phoneNumber = "xxxnumber"; // Replace with the desired phone number
 
       var url = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodedMessage;
 
       window.open(url);
     });
+  }
+}
 
-
+// Create WhatsApp button listeners
+createWhatsAppButtonListener("thalia-btn", "Hello, I want to buy the Thalia top crochet");
+createWhatsAppButtonListener("pink-btn", "Hello, I want to buy the Pink crochet top");
+createWhatsAppButtonListener("black-crochet-btn", "Hello, I want to buy the Black crochet top");
+createWhatsAppButtonListener("blue-Ropes-top-crochet-btn", "Hello, I want to buy the Blue Ropes top crochet");
+createWhatsAppButtonListener("halter-top-crochet-btn", "Hello, I want to buy the Halter top crochet");
+createWhatsAppButtonListener("Pink-Divide-crochet-set-btn", "Hello, I want to buy the Pink Divide crochet set");
+createWhatsAppButtonListener("Red-crochet-top-btn", "Hello, I want to buy the Red crochet top");
+createWhatsAppButtonListener("Deep-black-crochet-top-btn", "Hello, I want to buy the Deep black crochet top");
+createWhatsAppButtonListener("purple-divide-halter-top-btn", "Hello, I want to buy the Purple Divide halter top");
+createWhatsAppButtonListener("white-crochet-top-btn", "Hello, I want to buy the White crochet top");
+createWhatsAppButtonListener("orange-crochet-skirt-btn", "Hello, I want to buy the Orange crochet skirt");
+createWhatsAppButtonListener("fleece-mini-bag-crochet-btn", "Hello, I want to buy the Fleece mini bag crochet");
+createWhatsAppButtonListener("pink-divide-mesh-skirt-btn", "Hello, I want to buy the Pink Divide mesh skirt");
+createWhatsAppButtonListener("black-crochet-btn", "Hello, I want to buy the Black crochet top");
+createWhatsAppButtonListener("blue-white-tote-btn", "Hello, I want to buy the Blue and White tote");
+createWhatsAppButtonListener("pink-mermaid-btn", "Hello, I want to buy the Pink mermaid");
+createWhatsAppButtonListener("orange-mermaid-top-btn", "Hello, I want to buy the Orange mermaid top");
+createWhatsAppButtonListener("purple-blue-mermaid-top-btn", "Hello, I want to buy the Purple and Blue mermaid top");
+createWhatsAppButtonListener("redblue-cross-top-btn", "Hello, I want to buy the Red and Blue cross top");
+createWhatsAppButtonListener("checkered-bralette-top-crochet-btn", "Hello, I want to buy the Checkered Bralette top crochet");
+createWhatsAppButtonListener("blue-bridge-crochet-top-btn", "Hello, I want to buy the Blue Bridge crochet top");
+createWhatsAppButtonListener("fuzzy-purple-crochet-top-btn", "Hello, I want to buy the Fuzzy Purple crochet top");
+createWhatsAppButtonListener("checkered-mini-bag-btn", "Hello, I want to buy the Checkered mini bag");
+createWhatsAppButtonListener("grey-crochet-top-btn", "Hello, I want to buy the Grey crochet top");
+createWhatsAppButtonListener("green-brown-btn", "Hello, I want to buy the Green and Brown");
