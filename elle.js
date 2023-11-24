5c90725998e995d2365b34e135f3f9a49d8e631b
Jokibs <122084307+Jokibs@users.noreply.github.com>
2023-11-24 22:41:24 +0300

    Update elle.js

--- elle.js
+++ elle.js
@@ -1,53 +1,21 @@
-var i1 = 0;
-var images1 = ['purple-pom-pom-crochet-top.jpg','brown-divide-crochet-top.jpg', 'thalia-top-crochet.jpg', 'purple-half-pom-pom-crochet-top.jpg','peek-a-boob-top-crochet.webp', 'butterfly-top-crochet.webp','yellow-crochet-top.jpg','maren-top-crochet.jpg','orange-maren-top-crochet.jpg','pink-divide-top-crochet.jpg'];
-var time1 = 2000;
 
-function changeimg1() {
-    document.slide.src = images1[i1];
-    if (i1 < images1.length - 1) {
-        i1++;
-    } else {
-        i1 = 0;
+function changeImage(container, images, time) {
+    let i = 0;
+    function updateImage() {
+        if (container) {
+            container.src = images[i];
+            i = (i < images.length - 1) ? i + 1 : 0;
+            setTimeout(updateImage, time);
+        }
     }
-    setTimeout(changeimg1, time1);
+
+    updateImage();
 }
 
-var i1 = 0;
-var images1 = ['purple-pom-pom-crochet-top.jpg','brown-divide-crochet-top.jpg', 'thalia-top-crochet.jpg', 'purple-half-pom-pom-crochet-top.jpg','peek-a-boob-top-crochet.webp', 'butterfly-top-crochet.webp','yellow-crochet-top.jpg','maren-top-crochet.jpg','orange-maren-top-crochet.jpg','pink-divide-top-crochet.jpg'];
-var time1 = 2000;
-
-function changeimg1() {
-    document.slide.src = images1[i1];
-    if (i1 < images1.length - 1) {
-        i1++;
-    } else {
-        i1 = 0;
-    }
-    setTimeout(changeimg1, time1);
-}
-
-  var i2 = 0;
-var images2 = ['red-bikini-set.jpg', 'pink-white-bikini-set.jpg', 'blue-bikini-set.jpg'];
-var time2 = 4000;
-
-function changeimg2() {
-    document.two.src = images2[i2];
-    if (i2 < images2.length - 1) {
-        i2++;
-    } else {
-        i2 = 0;
-    }
-    setTimeout(changeimg2, time2);
-}
-
-window.onload = function() {
-
-
-    var changehome=document.getElementsByClassName("hamburger");changehome[1].innerHTML='<i class="material-icons">menu</i>';changehome[0].innerHTML='<i class="material-icons">menu</i>';changehome[0].style.color="white";changehome[0].style.fontSize="40px";
-
-changeimg1();
-changeimg2();
-};
+window.onload = function() {var changehome=document.getElementsByClassName("hamburger");changehome[1].innerHTML='<i class="material-icons">menu</i>';changehome[0].innerHTML='<i class="material-icons">menu</i>';changehome[0].style.color="white";changehome[0].style.fontSize="40px";
+changeImage(document.slide, images1, time1);changeImage(document.two, images2, time2);
+                           
+                           };
 
 //header scroll
 
