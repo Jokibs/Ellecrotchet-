5d0812bbcb0820eb8c0838fd2fd6aede87cdc795
Jokibs <122084307+Jokibs@users.noreply.github.com>
2023-11-24 21:33:37 +0300

    Update elle.js

--- elle.js
+++ elle.js
@@ -1,40 +1,30 @@
+function changeImage(containerId, images, time) {
+    let i = 0;
+    const container = document.getElementById(containerId);
 
-
-var i1 = 0;
-var images1 = ['purple-pom-pom-crochet-top.jpg','brown-divide-crochet-top.jpg', 'thalia-top-crochet.jpg', 'purple-half-pom-pom-crochet-top.jpg','peek-a-boob-top-crochet.webp', 'butterfly-top-crochet.webp','yellow-crochet-top.jpg','maren-top-crochet.jpg','orange-maren-top-crochet.jpg','pink-divide-top-crochet.jpg'];
-var time1 = 2000;
-
-function changeimg1() {
-    document.slide.src = images1[i1];
-    if (i1 < images1.length - 1) {
-        i1++;
-    } else {
-        i1 = 0;
+    function updateImage() {
+        container.src = images[i];
+        i = (i < images.length - 1) ? i + 1 : 0;
+        setTimeout(updateImage, time);
     }
-    setTimeout(changeimg1, time1);
+
+    updateImage();
 }
 
-  var i2 = 0;
-var images2 = ['red-bikini-set.jpg', 'pink-white-bikini-set.jpg', 'blue-bikini-set.jpg'];
-var time2 = 4000;
+// Images for the first slideshow
+const images1 = ['purple-pom-pom-crochet-top.jpg', 'brown-divide-crochet-top.jpg', 'thalia-top-crochet.jpg', 'purple-half-pom-pom-crochet-top.jpg', 'peek-a-boob-top-crochet.webp', 'butterfly-top-crochet.webp', 'yellow-crochet-top.jpg', 'maren-top-crochet.jpg', 'orange-maren-top-crochet.jpg', 'pink-divide-top-crochet.jpg'];
+const time1 = 2000;
 
-function changeimg2() {
-    document.two.src = images2[i2];
-    if (i2 < images2.length - 1) {
-        i2++;
-    } else {
-        i2 = 0;
-    }
-    setTimeout(changeimg2, time2);
-}
+
+// Images for the second slideshow
+const images2 = ['red-bikini-set.jpg', 'pink-white-bikini-set.jpg', 'blue-bikini-set.jpg'];
+const time2 = 4000;
+
+
  
-window.onload = function() {
-    
-    
-    var changehome=document.getElementsByClassName("hamburger");changehome[1].innerHTML='<i class="material-icons">menu</i>';changehome[0].innerHTML='<i class="material-icons">menu</i>';changehome[0].style.color="white";changehome[0].style.fontSize="40px";
-
-changeimg1();
-changeimg2();
+window.onload = function() {var changehome=document.getElementsByClassName("hamburger");changehome[1].innerHTML='<i class="material-icons">menu</i>';changehome[0].innerHTML='<i class="material-icons">menu</i>';changehome[0].style.color="white";changehome[0].style.fontSize="40px";
+changeImage('slide', images1, time1);
+changeImage('two', images2, time2);
 };
 
 //header scroll
@@ -54,16 +44,6 @@ window.onscroll = function() {
 };
 
 
-/*    // Set the time period in milliseconds
-  const timePeriod = 9000; // 9 seconds
-  
-  // Function to show the button
-  function showButton() {
-    document.getElementById("myButton").style.display = "block";
-  }
-  
-  // Set a timeout to show the button after the time period has elapsed
-  setTimeout(showButton, timePeriod); */
 
 
 
